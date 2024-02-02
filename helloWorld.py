fa6b50498dfc6618039e15e4e0e32799f2f5ed5c
David Stelzer <bxdstelzer@gmail.com>
2024-02-02 18:13:17 +0000

    Enhanced greeting based on time of day in helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,20 @@
+import datetime
+
 print('Hello, World!')
+
+# Asking for the user's name once
 name = input("Enter your name: ")
-print(f'Hello, {name}!')
+
+# Determining the current hour
+current_hour = datetime.datetime.now().hour
+
+# Determining the part of the day
+if 5 <= current_hour < 12:
+    part_of_day = 'morning'
+elif 12 <= current_hour < 18:
+    part_of_day = 'afternoon'
+else:
+    part_of_day = 'evening'
+
+# Printing a personalized greeting based on the time of day
+print(f'Good {part_of_day}, {name}!')
